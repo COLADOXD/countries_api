7d7c9bda6f8203c80d86eef1dff3be5a7b877448
MateoChacon <xdmateo90@gmail.com>
2023-04-25 10:10:09 -0500

    add assets and select colors in config tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,31 @@ module.exports = {
       fontFamily: {},
       textColor: (theme) => ({
         ...theme("colors"),
+        primary: "hsl(209, 23%, 22%)",
+
+        secondary: "hsl(207, 26%, 17%)",
+
+        terceary: "hsl(200, 15%, 8%)",
+
+        cuarto: "hsl(0, 0%, 52%)",
+
+        quinto: "hsl(0, 0%, 98%)",
+
+        sexto: "hsl(0, 0%, 100%)",
       }),
       backgroundColor: (theme) => ({
         ...theme("colors"),
+        primary: "hsl(209, 23%, 22%)",
+
+        secondary: "hsl(207, 26%, 17%)",
+
+        terceary: "hsl(200, 15%, 8%)",
+
+        cuarto: "hsl(0, 0%, 52%)",
+
+        quinto: "hsl(0, 0%, 98%)",
+
+        sexto: "hsl(0, 0%, 100%)",
       }),
     },
   },
