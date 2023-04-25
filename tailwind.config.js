4c29b690cdd9e76e2baf611e18592b653fb1c263
MateoChacon <xdmateo90@gmail.com>
2023-04-25 10:04:09 -0500

    chage in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {},
+      textColor: (theme) => ({
+        ...theme("colors"),
+      }),
+      backgroundColor: (theme) => ({
+        ...theme("colors"),
+      }),
+    },
   },
   plugins: [],
-}
-
+};
